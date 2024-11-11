f0a6f85c9ea41fa1da9c21f4cfd697684dc69809
Zak Le Bentz <zakerylebentz@gmail.com>
2024-11-11 15:40:59 +0000

    favourites page added, button started

---
+++ src/app/favourites/page.js
@@ -0,0 +1,14 @@
+import FavouriteButton from "@/components/FavouriteButton";
+
+export default function Favourites() {
+  return (
+    <>
+      <br />
+      <br />
+      <br />
+
+      <h1>These are your favourites</h1>
+      <FavouriteButton />
+    </>
+  );
+}

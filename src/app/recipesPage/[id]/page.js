a7e3f9f51da5e87773a1b8921ef9de738bc685b7
Katherine Day <kmday125@gmail.com>
2024-11-11 14:10:34 +0000

    error page, not-found page and single recipe page added

---
+++ src/app/recipesPage/[id]/page.js
@@ -0,0 +1,45 @@
+import { db } from "@/utils/dbConnection";
+// import pg from "pg";
+import Image from "next/image";
+
+export default async function SingleRecipePage({ params }) {
+  //   const db = new pg.Pool({
+  //     connectionString: process.env.NEXT_PUBLIC_DATABASE_URL,
+  //   });
+
+  const recipes = (
+    await db.query(`SELECT * FROM recipes WHERE id = ${params.id}`)
+  ).rows;
+  return (
+    <>
+      {recipes.map((recipe) => (
+        <div key={recipe.id} className="p-20">
+          <h2>RECIPE: {recipe.name}</h2>
+          <Image
+            src={recipe.image}
+            alt={recipe.name}
+            width={200}
+            height={200}
+          />
+          <ul>
+            <p>INGREDIENTS</p>
+            {recipe.ingredients.map((ingredient) => {
+              return (
+                <ul key={recipe.id + "-" + ingredient.name}>
+                  <li>
+                    {ingredient.amount} {ingredient.name} {ingredient.prep}
+                  </li>
+                </ul>
+              );
+            })}
+          </ul>
+          <p>INSTRUCTIONS: {recipe.instructions}</p>
+          <p>PREP TIME: {recipe.prep_time}</p>
+          <p>COOK TIME: {recipe.cook_time}</p>
+          <p>TOTAL TIME: {recipe.full_cook_time}</p>
+          <p>SERVINGS: {recipe.servings}</p>
+        </div>
+      ))}
+    </>
+  );
+}

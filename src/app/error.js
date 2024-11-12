b44439e9d746ad29f90c6d5c58801be2199a922e
Katherine Day <kmday125@gmail.com>
2024-11-12 21:35:10 +0000

    favourites rendering to favourites page, error and not found styled

--- src/app/error.js
+++ src/app/error.js
@@ -1,18 +1,29 @@
 "use client";
 import Link from "next/link";
+import Image from "next/image";
 
-export default function GlobalError({ error, reset }) {
+export default function GlobalError({ error }) {
   return (
-    <div>
-      <br />
-      <br />
-      <br />
-      <br />
-
-      <h2>Something went wrong globally!</h2>
-      <p>{error.message}</p>
-
-      <Link href="/">Go home</Link>
+    <div className="flex justify-center items-center min-h-screen">
+      <div className="card lg:card-side bg-base-100 shadow-xl max-w-xl">
+        <figure>
+          <Image
+            src="/ohDear.jpg"
+            alt="frustrated man, looking sad at his laptop"
+            height={400}
+            width={640}
+          />
+        </figure>
+        <div className="card-body">
+          <h2 className="card-title">Oh No! Something went wrong!</h2>
+          <p>{error.message}</p>
+          <div className="card-actions justify-end">
+            <Link className="btn btn-primary" href="/">
+              Home Page
+            </Link>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
--- src/app/not-found.js
+++ src/app/not-found.js
@@ -1,15 +1,28 @@
 import Link from "next/link";
+import Image from "next/image";
 
 export default function NotFoundPage() {
   return (
-    <>
-      <br />
-      <br />
-      <br />
-      <h1>Not found</h1>
-      <h2>This page does not exist</h2>
-      <br />
-      <Link href="/">Go Home</Link>
-    </>
+    <div className="flex justify-center items-center min-h-screen">
+      <div className="card lg:card-side bg-base-100 shadow-xl max-w-xl">
+        <figure>
+          <Image
+            src="/emptyPlate.jpg"
+            alt="empty plate, no food here"
+            height={400}
+            width={640}
+          />
+        </figure>
+        <div className="card-body">
+          <h2 className="card-title">No recipes found here</h2>
+          <p>This page does not exist</p>
+          <div className="card-actions justify-end">
+            <Link className="btn btn-primary" href="/">
+              Home Page
+            </Link>
+          </div>
+        </div>
+      </div>
+    </div>
   );
 }

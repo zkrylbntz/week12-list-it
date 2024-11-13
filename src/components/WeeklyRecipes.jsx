4f53c836b4091efe13b7bf9b7a45a45898e7e913
Anu Heese <anuheese@gmail.com>
2024-11-13 10:01:18 +0000

    weekly links

--- src/components/WeeklyRecipes.jsx
+++ src/components/WeeklyRecipes.jsx
@@ -2,6 +2,7 @@ import Image from "next/image";
 import { db } from "@/utils/dbConnection";
 import { auth } from "@clerk/nextjs/server";
 import getCurrentSessionId from "@/utils/currentSession";
+import Link from "next/link";
 
 export default async function WeeklyRecipes() {
   const { userId } = await auth();
@@ -21,12 +22,14 @@ WHERE user_clerk_id = $1 AND  session_id = $2 ;`,
       {recipes.map((recipe) => {
         return (
           <div key={recipe.id}>
-            <Image
-              src={recipe.image}
-              alt={recipe.name}
-              width={200}
-              height={200}
-            />
+            <Link href={`/recipesPage/${recipe.id}`}>
+              <Image
+                src={recipe.image}
+                alt={recipe.name}
+                width={200}
+                height={200}
+              />
+            </Link>
             <h2>{recipe.name}</h2>
           </div>
         );

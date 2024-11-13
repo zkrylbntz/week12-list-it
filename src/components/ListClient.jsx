aa5d89008347250fee4f1ddefdb5f9ed89f808ce
Zak Le Bentz <zakerylebentz@gmail.com>
2024-11-13 19:26:36 +0000

    Ingredients list added to a card

--- src/components/ListClient.jsx
+++ src/components/ListClient.jsx
@@ -1,9 +1,9 @@
-import Image from "next/image";
+// import Image from "next/image";
 import { db } from "@/utils/dbConnection";
 import { auth } from "@clerk/nextjs/server";
 import getCurrentSessionId from "@/utils/currentSession";
 
-import "./listclient.css";
+// import "./listclient.css";
 
 export default async function List() {
   const { userId } = await auth();
@@ -19,33 +19,54 @@ WHERE user_clerk_id = $1 AND  session_id = $2 ;`,
 
   const recipes = recipes_results.rows;
   return (
-    <div className="content-container">
-      {recipes.map((recipe) => {
-        return (
-          <div className="content" key={recipe.id}>
-            {recipe.ingredients.map((ingredient) => {
+    // <div className="content-container">
+    // {recipes.map((recipe) => {
+    //   return (
+    //     <div className="content" key={recipe.id}>
+    //       {recipe.ingredients.map((ingredient) => {
+    //         return (
+    //           <ul
+    //             className="list-text"
+    //             key={recipe.id + "-" + ingredient.name}
+    //           >
+    //             <li>
+    //               <input type="checkbox" className="checkbox" />
+    //               &nbsp;&nbsp;
+    //               {ingredient.amount} {ingredient.name} {ingredient.prep}
+    //             </li>
+    //           </ul>
+    //         );
+    //       })}
+    //     </div>
+    //   );
+    // })}
+    // </div>
+    <>
+      <div className="flex flex-col items-center">
+        <div className="card bg-orange-800 w-96 shadow-xl">
+          <div className="card-body">
+            <h2 className="card-title">Ingredients List</h2>
+            {recipes.map((recipe) => {
               return (
-                <ul
-                  className="list-text"
-                  key={recipe.id + "-" + ingredient.name}
-                >
-                  <li>
-                    <input type="checkbox" className="checkbox" />
-                    &nbsp;&nbsp;
-                    {ingredient.amount} {ingredient.name} {ingredient.prep}
-                  </li>
-                </ul>
+                <div key={recipe.id}>
+                  {recipe.ingredients.map((ingredient) => {
+                    return (
+                      <ul key={recipe.id + "-" + ingredient.name}>
+                        <li>
+                          <input type="checkbox" className="checkbox" />
+                          &nbsp;&nbsp;
+                          {ingredient.amount} {ingredient.name}{" "}
+                          {ingredient.prep}
+                        </li>
+                      </ul>
+                    );
+                  })}
+                </div>
               );
             })}
           </div>
-        );
-      })}
-    </div>
+        </div>
+      </div>
+    </>
   );
 }
-
-<div className="card bg-base-100 w-96 shadow-xl">
-  <div className="card-body">
-    <h2 className="card-title">Ingredients List</h2>
-  </div>
-</div>;
